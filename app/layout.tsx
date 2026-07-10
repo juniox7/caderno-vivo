939c18afe1259283ebdafd5e444b4d0eddfba17d
Desenvolvedor <dev@example.com>
2026-07-10 07:28:04 -0300

    feat: adiciona meta pixel e documentacoes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
 import { Toaster } from "sonner";
+import Script from "next/script";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -62,6 +63,30 @@ export default function RootLayout({
             {children}
             <Toaster position="top-right" richColors closeButton />
           </ThemeProvider>
+          
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1489450116541113');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1489450116541113&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
         </body>
       </html>
     </ClerkProvider>
